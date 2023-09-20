f52d6152277c0c6c5050d36de9cbe5a99c86858c
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2023-09-20 23:57:29 +0100

    unit tests

--- UnitTests/MarkMyFinances.UnitTesting/ExpenseServiceTests.cs
+++ UnitTests/MarkMyFinances.UnitTesting/ExpenseServiceTests.cs
@@ -18,6 +18,7 @@ namespace MarkMyFinances.UnitTesting
 
 		// objects required for test
 		private Expense _expense;
+		private ExpenseDto _expenseDto;
 
 		public ExpenseServiceTests()
 		{
@@ -43,6 +44,54 @@ namespace MarkMyFinances.UnitTesting
 				TransactionDate = new DateTime().AddDays(2),
 				Value = 100
 			};
+
+			_expenseDto = new ExpenseDto()
+			{
+				Id = 1,
+				CategoryId = 1,
+				CreatedAt = DateTime.UtcNow,
+				UpdatedAt = DateTime.UtcNow,
+				Description = "UNKNOWN",
+				SubCategoryId = 1,
+				TransactionDate = new DateTime().AddDays(2),
+				Value = 100
+			};
+		}
+
+		[Test]
+		public async Task CreatesExpense_ReceivesCorrectEntity_ReturnsTrue()
+		{
+			_expense.Id = 1;
+			_expenseDto.Id = 1;
+
+			_unitOfWork.Setup(exp => exp.ExpensesRepository.CreateAsync(_expense)).ReturnsAsync(true);
+
+			var result = await _expenseService.AddAsync(_expenseDto);
+
+			Assert.That(result, Is.EqualTo(true));
+		}
+
+		[Test]
+		public async Task EditsExpense_ReceivesCorrectEntity_ReturnsTrue()
+		{
+			_expense.Description = "NEW OBJ";
+			_expenseDto.Description = "NEW OBJ";
+
+			_unitOfWork.Setup(exp => exp.ExpensesRepository.UpdateAsync(_expense)).ReturnsAsync(true);
+
+			var result = await _expenseService.EditAsync(_expenseDto);
+
+			Assert.That(result, Is.EqualTo(true));
+		}
+
+		[Test]
+		public async Task RemovesExpense_ReceivesCorrectEntity_ReturnsTrue()
+		{	
+			_unitOfWork.Setup(exp => exp.ExpensesRepository.DeleteAsync(_expense)).ReturnsAsync(true);
+
+			var result = await _expenseService.RemoveAsync(_expenseDto);
+
+			Assert.That(result, Is.EqualTo(true));
 		}
 
 		[Test]
